dc4096f1e68396f840c704c90052853f1c46f580
Tobi <tohbyyy@gmail.com>
2024-05-14 23:46:18 +0700

    Update header link for mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="/nidovn-logo-7.png" class="logo" alt="Nido Logo">
+            <img src="{{ asset('/nidovn-logo-7.png') }}" class="logo" alt="Nido Logo">
         </a>
     </td>
 </tr>
