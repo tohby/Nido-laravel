a5b5a74d3f70c3cc476ec2f8ffa3fdcc87ebdad1
Tobi.adeyinka <tobi.adeyinka@nestle.com>
2024-05-14 12:27:33 +0700

    Update pages: Contact, ABout

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import Guest from "@/Layouts/GuestLayout";
 import { Head, Link } from "@inertiajs/react";
+import { faqs } from "@/Constants/faqs";
+import { Disclosure, Transition } from "@headlessui/react";
+import { ChevronUp, Users } from "react-feather";
+
 
 const About = () => {
     return (
         <Guest>
             <Head title="About us" />
             <header>
-                <div className="mx-auto max-w-screen-3xl px-4 lg:px-2.5 flex flex-col items-center py-12 md:py-18">
+                <div className="mx-auto max-w-screen-3xl px-4 lg:px-6 flex flex-col items-center py-12 md:py-18">
                     <div className="flex flex-col items-center text-center lg:max-w-[66.67%]">
                         <h1 className="headline mb-4 whitespace-pre-wrap text-balance text-black theme-tech:text-white lg:mb-8">
-                            Get going with Agent OS.
+                            About NIDO
                         </h1>
-                        <p className="body-m md:body-l lg:title-s max-w-xl whitespace-pre-wrap text-pretty text-gray-300">
-                            In the age of conversational AI, every company needs
-                            an agent. Agent OS makes it easy.
-                        </p>
+                        {/* <p className="body-m md:body-l lg:title-s max-w-xl whitespace-pre-wrap text-pretty text-gray-300">
+                            Who we are
+                        </p> */}
                     </div>
                     <div className="relative mt-12 aspect-[3/4] w-full md:aspect-[16/7]">
-                        <figure className="relative h-full w-full overflow-hidden rounded-3xl bg-gray-200 [transform:translateZ(0)] theme-tech:bg-gray-700 theme-platform:bg-gray-100 md:rounded-4xl">
+                        <figure className="relative h-full w-full overflow-hidden rounded-3xl bg-gray-200 [transform:translateZ(0)]">
                             <img
-                                alt="Person using a laptop"
+                                alt="Nido"
                                 fetchpriority="high"
                                 decoding="async"
                                 data-nimg="fill"
                                 className="block h-auto w-full object-cover"
                                 sizes="95vw"
-                                srcSet="https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=640&quality=90 640w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=750&quality=90 750w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=828&quality=90 828w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=1080&quality=90 1080w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=1200&quality=90 1200w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=1920&quality=90 1920w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=2048&quality=90 2048w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=3840&quality=90 3840w"
-                                src="https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Ff08db6e7358cbf1780c8f2a6dddf6d20484811d9-4500x1888.jpg&width=3840&quality=90"
+                                src="/Nido-12.jpg"
                                 style={{
                                     position: "absolute",
                                     height: "100%",
@@ -37,27 +39,114 @@ const About = () => {
                                     color: "transparent",
                                 }}
                             />
-                            <div style={{ opacity: 1 }}>
-                                <div className="absolute overflow-hidden rounded-2xl bottom-2 left-2 md:bottom-6 md:left-6 w-[calc((2/3)*100%)] md:w-48 lg:w-80 2xl:w-96">
-                                    <img
-                                        alt="Examples features of Agent OS, such as Summaries, Alerts, Knowledge and Integrations"
-                                        loading="lazy"
-                                        width={4000}
-                                        height={2600}
-                                        decoding="async"
-                                        data-nimg={1}
-                                        className="block h-auto w-full"
-                                        sizes="(min-width: 1025px) 320px, 192px"
-                                        srcSet="https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=16&quality=90 16w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=32&quality=90 32w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=48&quality=90 48w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=64&quality=90 64w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=96&quality=90 96w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=128&quality=90 128w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=256&quality=90 256w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=384&quality=90 384w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=640&quality=90 640w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=750&quality=90 750w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=828&quality=90 828w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=1080&quality=90 1080w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=1200&quality=90 1200w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=1920&quality=90 1920w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=2048&quality=90 2048w, https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=3840&quality=90 3840w"
-                                        src="https://sierra.ai/-/cdn/image?src=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fca4jck6w%2Fproduction%2Fdcfe752d08c53374820a842afa4eee7c5b0945bf-4000x2600.png&width=3840&quality=90"
-                                        style={{ color: "transparent" }}
-                                    />
-                                </div>
-                            </div>
+
                         </figure>
                     </div>
                 </div>
             </header>
+            <section
+                className="theme-product relative bg-white py-12 text-gray-400 theme-tech:bg-black theme-tech:text-gray-100 theme-product:bg-gray-100 theme-platform:bg-gray-200 md:py-16 lg:py-18"
+                style={{ zIndex: 6 }}
+            >
+                <div className="mx-auto max-w-screen-3xl px-4 lg:px-2.5">
+                    <div className="grid grid-cols-12 gap-2 md:gap-2.5 pb-8 md:pb-14 lg:pb-18">
+                        <div className="col-span-12 flex flex-col items-start md:col-span-10 lg:col-span-7 lg:col-start-2">
+                            <h2 className="text-5xl text-pretty pr-4 text-black theme-tech:text-white md:pr-0">
+                                Intuitive and authentic conversations{" "}
+                            </h2>
+                            <p className="body-m mt-2 text-pretty pr-4 text-gray-500 md:max-w-[90%] md:pr-0 lg:mt-6">
+                                The Nigerians in Diaspora Organisation Vietnam is the Vietnamese arm of a global Nigerian Diaspora network. Established in the year 2000 and registered in England and Wales in 2002 under the Companies Act, the organization’s Headquarters is in London.
+
+                                NIDO VN provides an umbrella to all Nigerians in Vietnam. In concert with the other continental arms of NIDO in the Americas, Asia and Africa, the organization is the largest assembly of Nigerians worldwide.
+
+                                The Government of Nigeria recognizes the organization as an official platform through which individual Nigerian Diaspora, their Community Organizations, and Corporate Bodies can channel their developmental efforts to Nigeria. In this sense, the organization partners with Nigerian Community / Professional Organizations as well as public and private businesses in focused areas such as Foreign Direct Investment (FDI), professional networking, stakeholder advocacy, medical missions, educational support and skills transfer to Nigeria.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="CTA" className="mx-4 mt-2 lg:mt-20">
+                <div className="relative bg-green-900 py-80 rounded-xl">
+                    <img
+                        src="/cta_bg.png"
+                        alt="Background Image"
+                        className="absolute inset-0 w-full h-full object-cover"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <div className="mx-auto max-w-5xl px-6">
+                            <div className="text-white text-center">
+                                <h2 className="text-3xl lg:text-6xl font-normal">
+                                    Join us and be part of a community where
+                                    camaraderie meets opportunity for Nigerians
+                                    in Vietnam.
+                                </h2>
+                                <div className="mt-8">
+                                    <Link
+                                        href={route("member.join")}
+                                        type="button"
+                                        className="focus:outline-none text-white bg-transparent border-2 border-white hover:bg-white hover:text-green-800 focus:ring-4 focus:ring-green-300 font-medium text-lg px-6 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 rounded-lg hover:-translate-y-1 transition ease-in-out delay-150"
+                                    >
+                                        Become a member
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="faqs">
+                <div className="container py-20 lg:py-40 mx-auto">
+                    <div className="max-w-xl mx-auto">
+                        <div className="w-full h-1 max-w-28 min-h-1 bg-green-700 mb-4 mx-auto"></div>
+                        <div className="text-center mb-10">
+                            <h2 className="text-6xl font-normal">
+                                Frequently Asked Questions
+                            </h2>
+                        </div>
+                    </div>
+                    <div className="max-w-3xl mx-auto">
+                        <div className="w-full pt-2">
+                            <div className="mx-auto w-full rounded-2xl bg-white p-2">
+                                {faqs.map((faq, index) => (
+                                    <Disclosure
+                                        defaultOpen={index === 0 && true}
+                                        as="div"
+                                        className={index !== 0 ? "mt-6" : ""}
+                                        key={index}
+                                    >
+                                        {({ open }) => (
+                                            <>
+                                                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-green-100 px-6 py-6 text-left text-2xl font-normal text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring focus-visible:ring-green-500/75">
+                                                    <span>{faq.question}</span>
+                                                    <ChevronUp
+                                                        className={`${open
+                                                            ? "rotate-180 transform transition-transform duration-300"
+                                                            : "transition-transform duration-300"
+                                                            } h-10 w-10 text-green-500`}
+                                                    />
+                                                </Disclosure.Button>
+                                                <Transition
+                                                    enter="transition duration-100 ease-out"
+                                                    enterFrom="transform scale-95 opacity-0"
+                                                    enterTo="transform scale-100 opacity-100"
+                                                    leave="transition duration-75 ease-out"
+                                                    leaveFrom="transform scale-100 opacity-100"
+                                                    leaveTo="transform scale-95 opacity-0"
+                                                >
+                                                    <Disclosure.Panel className="px-4 pb-2 pt-4 text-xl text-gray-500">
+                                                        {faq.answer}
+                                                    </Disclosure.Panel>
+                                                </Transition>
+                                            </>
+                                        )}
+                                    </Disclosure>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Guest>
     );
 };
--- resources/js/Pages/Contact.jsx
+++ resources/js/Pages/Contact.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Guest from "@/Layouts/GuestLayout";
 import { Head, Link } from "@inertiajs/react";
+import { Facebook, MessageCircle, Phone } from "react-feather";
+import { faqs } from "@/Constants/faqs";
+import { Disclosure, Transition } from "@headlessui/react";
+import { ChevronUp, Users } from "react-feather";
 
 const About = () => {
     return (
         <Guest>
             <Head title="Contact us" />
-            <header>
+            <header className="mb-4">
                 <div className="mx-auto max-w-screen-3xl px-4 lg:px-2.5 flex flex-col items-center py-12 md:py-18">
                     <div className="flex flex-col items-center text-center lg:max-w-[66.67%]">
-                        <h1 className="headline mb-4 whitespace-pre-wrap text-balance text-black theme-tech:text-white lg:mb-8">
+                        <h1 className="headline mb-4 whitespace-pre-wrap text-balance text-black lg:mb-8">
                             Get in touch with Us.
                         </h1>
                         <p className="text-lg max-w-xl whitespace-pre-wrap text-pretty text-gray-500">
@@ -19,9 +23,161 @@ const About = () => {
                             soon as possible.
                         </p>
                     </div>
-                    <div className="relative mt-12 aspect-[3/4] w-full md:aspect-[16/7]"></div>
                 </div>
             </header>
+            <section className="px-6 max-w-6xl container mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                    <div className="max-w-sm p-6 bg-white border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-700 mx-auto">
+                        <div className="flex items-center justify-center w-12 h-12 rounded-md bg-green-100 mb-4">
+                            <Phone className="text-green-900" />
+                        </div>
+
+                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                            Call us
+                        </h5>
+                        <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                            Our lines are open,
+                            Don't hesitate to give us a call.
+                        </p>
+                        <h5
+
+                            className="inline-flex font-medium items-center text-green-600 mt-12"
+                        >
+                            +84775568278
+                        </h5>
+                    </div>
+                    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 mx-auto">
+                        <div className="flex items-center justify-center w-12 h-12 rounded-md bg-green-100 mb-4">
+                            <MessageCircle className="text-green-900" />
+                        </div>
+                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                            Message Us
+                        </h5>
+                        <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                            Send us an email, and our dedicated team will get back to you promptly.
+                        </p>
+                        <h5
+                            className="inline-flex font-medium items-center text-green-600 mt-12"
+                        >
+                            info@nidovietnam.com
+                        </h5>
+                    </div>
+                    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 mx-auto">
+                        <div className="flex items-center justify-center w-12 h-12 rounded-md bg-green-100 mb-4">
+                            <Facebook className="text-green-900" />
+                        </div>
+                        <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                            Join our community
+                        </h5>
+                        <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                            Connect with like-minded individuals, share ideas, and stay updated on the latest news and events by joining our Facebook group
+                        </p>
+                        <a
+                            href="https://www.facebook.com/groups/357099351095953"
+                            target="_blank"
+                            className="inline-flex font-medium items-center text-green-600 hover:underline"
+                        >
+                            Nigerians in Vietnam facebook
+                            <svg
+                                className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 18 18"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+                                />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <section id="CTA" className="mx-4 mt-2 lg:mt-20">
+                <div className="relative bg-green-900 py-80 rounded-xl">
+                    <img
+                        src="/cta_bg.png"
+                        alt="Background Image"
+                        className="absolute inset-0 w-full h-full object-cover"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <div className="mx-auto max-w-5xl px-6">
+                            <div className="text-white text-center">
+                                <h2 className="text-3xl lg:text-6xl font-normal">
+                                    Join us and be part of a community where
+                                    camaraderie meets opportunity for Nigerians
+                                    in Vietnam.
+                                </h2>
+                                <div className="mt-8">
+                                    <Link
+                                        href={route("member.join")}
+                                        type="button"
+                                        className="focus:outline-none text-white bg-transparent border-2 border-white hover:bg-white hover:text-green-800 focus:ring-4 focus:ring-green-300 font-medium text-lg px-6 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 rounded-lg hover:-translate-y-1 transition ease-in-out delay-150"
+                                    >
+                                        Become a member
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="faqs">
+                <div className="container py-20 lg:py-40 mx-auto">
+                    <div className="max-w-xl mx-auto">
+                        <div className="w-full h-1 max-w-28 min-h-1 bg-green-700 mb-4 mx-auto"></div>
+                        <div className="text-center mb-10">
+                            <h2 className="text-6xl font-normal">
+                                Frequently Asked Questions
+                            </h2>
+                        </div>
+                    </div>
+                    <div className="max-w-3xl mx-auto">
+                        <div className="w-full pt-2">
+                            <div className="mx-auto w-full rounded-2xl bg-white p-2">
+                                {faqs.map((faq, index) => (
+                                    <Disclosure
+                                        defaultOpen={index === 0 && true}
+                                        as="div"
+                                        className={index !== 0 ? "mt-6" : ""}
+                                        key={index}
+                                    >
+                                        {({ open }) => (
+                                            <>
+                                                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-green-100 px-6 py-6 text-left text-2xl font-normal text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring focus-visible:ring-green-500/75">
+                                                    <span>{faq.question}</span>
+                                                    <ChevronUp
+                                                        className={`${open
+                                                            ? "rotate-180 transform transition-transform duration-300"
+                                                            : "transition-transform duration-300"
+                                                            } h-10 w-10 text-green-500`}
+                                                    />
+                                                </Disclosure.Button>
+                                                <Transition
+                                                    enter="transition duration-100 ease-out"
+                                                    enterFrom="transform scale-95 opacity-0"
+                                                    enterTo="transform scale-100 opacity-100"
+                                                    leave="transition duration-75 ease-out"
+                                                    leaveFrom="transform scale-100 opacity-100"
+                                                    leaveTo="transform scale-95 opacity-0"
+                                                >
+                                                    <Disclosure.Panel className="px-4 pb-2 pt-4 text-xl text-gray-500">
+                                                        {faq.answer}
+                                                    </Disclosure.Panel>
+                                                </Transition>
+                                            </>
+                                        )}
+                                    </Disclosure>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Guest>
     );
 };
